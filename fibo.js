cc4ad87fb70356c39ca4c5650c5942adb37ce202
Maksym Chertenkov <max.chertenkov@gmail.com>
2016-04-15 00:33:35 +0300

    fibo propepty

---
+++ fibo.js
@@ -0,0 +1,20 @@
+// возвращает массив заданой длинны
+
+Object.defineProperty(Number.prototype, "fibo", {
+	get: function() {
+		var length = this
+		var result = [1, ]
+		var num = 1
+		var i = 0
+		while (result.length < length) {
+			result.push(num)
+			num = result[i] + result[i+1]
+			i++
+		}
+		return result
+	},
+	enumerable:false,
+	configutable:true
+})
+
+13..fibo // [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233]
